73bffb193c2ec43aa2c92d387a69618582df7893
Sato12 <50491191+Sato12@users.noreply.github.com>
2019-07-02 16:49:58 -0500

    Add files via upload

--- FMS_Proyecto/i2s.py
+++ FMS_Proyecto/i2s.py
@@ -24,131 +24,138 @@ class FilterClock(Module, AutoCSR):
 
 class BitClock(Module, AutoCSR):
     def __init__(self, counter, cyc_lvl, bck):
-        self.comb += bck.eq(counter < cyc_lvl)
+        self.comb += bck.eq(counter > cyc_lvl)
 
 class WordSelector(Module, AutoCSR):
-    def __init__(self, counter, bit_word, sd):
-        self.comb += lck.eq(counter < bit_word)
+    def __init__(self, counter, bit_word, ws):
+        self.comb += ws.eq(counter >= bit_word)
 
 class _I2S(Module, AutoCSR):
-    def __init__(self, flt, dmp, scl, bck, sd, ws, fmt, xmt, clk_freq):                                             # flt, dmp, scl, bck, din, lck, fmt, xmt):
-        # sinusoidal_400 = {}
-        # for i in range(44):
-        #     sinusoidal_400[i] = data_left.eq(int(round((math.sin(math.pi*2*1000*i/44100)+1)*16383)))#, data_right.eq(int(round(math.sin(i/44100)*32767))))
-        #     print (int(round((math.sin(math.pi*2*1000*i/44100)+1)*16383)))
+    def __init__(self, flt, dmp, scl, bck, sd, ws, fmt, xmt):                                             # flt, dmp, scl, bck, din, lck, fmt, xmt):
+
 ##############################################################################################################
         self.width_word = width_word = Signal(6)
-        self.sample_frecuency = sample_frecuency = Signal(16)
+        self.divisor_bck = divisor_bck = Signal(7)
+        self.divisor_scl = divisor_scl = Signal(5)
         self.start = start = Signal()
         self.counter_bck = counter_bck = Signal(7)
         self.counter_scl = counter_scl = Signal(5)
-        self.counter_word = counter_word = Signal(6)
+        self.counter_ws = counter_ws = Signal(5)
+        self.counter_word = counter_word = Signal(5)
         self.load_buffer = load_buffer = Signal()
         self.load_bit = load_bit = Signal()
+        self.load_muestreo = load_muestreo = Signal()
         self.data_left = data_left = Signal(32)
         self.data_right = data_right = Signal(32)
         self.buffer = buffer = Signal(32)
-        self.clk_bck = clk_bck = Signal(7)
-        self.clk_scl = clk_scl = Signal(5)
-
+        self.channel = channel = Signal()
+        self.j = j = Signal(6)
         # ingrese la lógica respectiva  para el periferico
+
+
+        sinusoidal_1000 = {}
+        for i in range(44):
+            sinusoidal_1000[i] =data_left.eq(int(round((math.sin(math.pi*2*1000*i/44100)+1)*16383)))
+            print (int(round((math.sin(math.pi*2*1000*i/44100)+1)*16383)))
+
+
+
         #########################################################################################
         self.comb += [
-            load_bit.eq(counter_bck == clk_bck-1),
-            load_buffer.eq((counter_word == width_word) | (counter_word == width_word*2))
+
+            width_word.eq(16),
+            divisor_bck.eq(35),
+            divisor_scl.eq(9),
+
+            load_bit.eq(counter_bck == divisor_bck*2-1),
+            load_buffer.eq(counter_word == width_word),
+            load_muestreo.eq((counter_ws == width_word*2-1) & load_bit),
+            channel.eq(counter_ws >= width_word),
+
+            If(~self.channel,
+                Case(j,sinusoidal_1000)
+            )
         ]
         #########################################################################################
         self.sync += [
             # Contador para Reloj de datos
             counter_bck.eq(counter_bck+1),
-            If(counter_bck == clk_bck-1,
+            If(counter_bck == divisor_bck*2-1,
                 counter_bck.eq(0)
             ),
 
             # Contador para Reloj de Filtro
             counter_scl.eq(counter_scl+1),
-            If(counter_scl == clk_scl-1,
+            If(counter_scl == divisor_scl*2-1,
                 counter_scl.eq(0)
+            ),
+            #Contador de Muestreo
+            j.eq(j+load_muestreo),
+            If(j == 44,
+                j.eq(0)
             )
         ]
         ########################################################################################3
         self.submodules.i2s_fsm = FSM(reset_state="IDLE")
         self.i2s_fsm.act("IDLE",
-            If(start,
-                NextState("CONFIG")
-            )
+            #If(start,
+                NextState("RESET_CLOCKS")
+            #)
         )
-        self.i2s_fsm.act("CONFIG",
-            If(width_word == 16,
-                If(sample_frecuency == 48000,
-                    NextValue(clk_bck, 66),
-                    NextValue(clk_scl, 16),
-                    NextState("CLOCKS")
-                ).Elif(sample_frecuency == 44100,
-                    NextValue(clk_bck, 70),
-                    NextValue(clk_scl, 18),
-                    NextState("CLOCKS")
-                )
-            ).Elif(width_word == 32,
-                If(sample_frecuency == 48000,
-                    NextValue(clk_bck, 32),
-                    NextValue(clk_scl, 16),
-                    NextState("CLOCKS")
-                ).Elif(sample_frecuency == 44100,
-                    NextValue(clk_bck, 36),
-                    NextValue(clk_scl, 18),
-                    NextState("CLOCKS")
-                )
-            )
-        )
-        self.i2s_fsm.act("CLOCKS",
+
+        self.i2s_fsm.act("RESET_CLOCKS",
             NextValue(counter_bck, 0),
             NextValue(counter_scl, 0),
-            NextValue(counter_word, 0),
+            NextValue(counter_ws, 0),
             NextState("LOAD")
         )
-        self.i2s_fsm.act("DATOS",
+        self.i2s_fsm.act("SERIALIZAR",
             If(load_bit,
                 If(width_word == 16,
                     NextValue(sd, buffer[15]),
-                    NextValue(buffer, Cat(buffer[30:0],0))
+                    NextValue(buffer, Cat(0,buffer[0:30]))
                     #sd.eq(Mux(counter_word,buffer[15],buffer[15],buffer[15],buffer[12],buffer[11],buffer[10],buffer[9],buffer[8],buffer[7],buffer[6],buffer[5],buffer[4],buffer[3],buffer[2],buffer[1],buffer[0]))
                 ).Elif(width_word == 32,
                     NextValue(sd, buffer[31]),
-                    NextValue(buffer, Cat(buffer[30:0],0))
+                    NextValue(buffer, Cat(0,buffer[0:30]))
                     #sd.eq(Mux(counter_word,buffer[31]))
-                ).Else(
-                    print("Error en el tamaño de bits")
-                ),
+                ),#.Else(
+                #    print("Error en el tamaño de bits")
+                #),
                 NextValue(counter_word,counter_word+1),
-                
+                NextValue(counter_ws,counter_ws+1)
+
             ),
             If(load_buffer,
-                    NextState("LOAD")
-            ),
-        ),
+                NextState("LOAD")
+            )
+        )
         self.i2s_fsm.act("LOAD",
-            If(counter_word[4], #Para el caso de width_word = 16
-                NextValue(buffer, data_right),
-            ).Else(
-                NextValue(buffer, data_left)
-            ),
-            NextState("DATOS")
+            #If(channel,
+            #    NextValue(buffer, data_right),
+            #).Else(
+            #    NextValue(buffer, data_left)
+            #),
+            NextValue(buffer,Mux(channel, data_right, data_left)),
+            NextValue(counter_word, 0),
+            NextState("SERIALIZAR")
         )
         ##########################################################################################3
-        self.submodules.bitClock = BitClock(counter_bck, 35, bck)
-        self.submodules.filClock = FilterClock(counter_scl, 9, scl)
-
+        self.submodules.bitClock = BitClock(counter_bck, divisor_bck, bck)
+        self.submodules.filClock = FilterClock(counter_scl, divisor_scl, scl)
+        self.submodules.wordSelect = WordSelector(counter_ws, width_word, ws)
+        self.submodules.configuracion = Configuracion(flt, dmp, fmt, xmt)
 
 
 class I2S(Module, AutoCSR):
-    def __init__(self, flt, dmp, scl, bck, sd, ws, fmt, xmt, clk_freq):                                    #flt, dmp, scl, bck, din, lck, fmt, xmt):
+    def __init__(self, flt, dmp, scl, bck, sd, ws, fmt, xmt):                                    #flt, dmp, scl, bck, din, lck, fmt, xmt):
         self.Width_word = CSRStorage(32)
-        self.Sample_Frecuency = CSRStorage(32)
-        self.En_left = CSRStatus()
-        self.En_right = CSRStatus()
-        self.data_left = CSRStorage(16)
-        self.data_right = CSRStorage(16)
+        self.Divisor_BCK = CSRStorage(7)
+        self.Divisor_SCL = CSRStorage(5)
+        #self.En_left = CSRStatus()
+        #self.En_right = CSRStatus()
+        self.data_left = CSRStorage(32)
+        self.data_right = CSRStorage(32)
         self.Start = CSRStorage()
 
         # # #
@@ -157,15 +164,16 @@ class I2S(Module, AutoCSR):
         #self.ev.zero = EventSourcePulse()
         #self.ev.finalize()
 
-        _i2s = _I2S(flt, dmp, scl, bck, sd, ws, fmt, xmt, clk_freq)                                            #flt, dmp, scl, bck, din, lck, fmt, xmt)
+        _i2s = _I2S(flt, dmp, scl, bck, sd, ws, fmt, xmt)                                            #flt, dmp, scl, bck, din, lck, fmt, xmt)
         self.submodules += _i2s
 
         self.comb += [
            _i2s.width_word.eq(self.Width_word.storage),
-           _i2s.sample_frecuency.eq(self.Sample_Frecuency.storage),
-           _i2s.start.eq(self.Start.storage)
-           #_i2s.data_left.eq(self.data_left.storage),
-           #_i2s.data_right.eq(self.data_right.storage),
+           _i2s.divisor_bck.eq(self.Divisor_BCK.storage),
+           _i2s.divisor_scl.eq(self.Divisor_SCL.storage),
+           _i2s.start.eq(self.Start.storage),
+           _i2s.data_left.eq(self.data_left.storage),
+           _i2s.data_right.eq(self.data_right.storage),
            #self.En_left.status.eq(_i2s.en_left_sw),
            #self.En_right.status.eq(_i2s.en_right_sw),
         ]
@@ -179,11 +187,15 @@ if __name__ == '__main__':
     dmp = Signal()
     fmt = Signal()
     xmt = Signal()
-    dut = _I2S(flt, dmp, scl, bck, sd, ws, fmt, xmt, 100000000)
+    dut = _I2S(flt, dmp, scl, bck, sd, ws, fmt, xmt)
 
     def dut_tb(dut):
+        #yield dut.data_left.eq(10986)
+        #yield dut.data_right.eq(30901)
         yield dut.width_word.eq(16)
-        yield dut.sample_frecuency.eq(44100)
+        yield dut.divisor_bck.eq(35)
+        yield dut.divisor_scl.eq(9)
+        #yield dut.start.eq(1)
         for i in range(10000):
         	yield
 
